197e0d6d0492bdc5b1dfab7ae012852f2b8af55c
“Kevin-Jimenez-D” <kevinjimenezd97@gmail.com>
2023-09-27 10:58:33 -0500

    docs: :memo: DOcumente mejor
    Por temas de tiempo lo dejo así

--- storage/v4.js
+++ storage/v4.js
@@ -1,5 +1,6 @@
 //EN el readme, si tratamos de buscar en v3 "https://docs.spacexdata.com/" ->Rockets -> All rockets y buscar en el JSON (view more) images, no aparece nada, entonces se mira en v4
-
+//https://github.com/r-spacex/SpaceX-API/tree/master/docs#rspacex-api-docs esto es el v4, voy a rockets/v4 ->query.md (porque hay esta flickr_images)
+//""  flickr_images segun el readme "https://github.com/r-spacex/SpaceX-API/blob/master/docs/rockets/v4/query.md" con el body dice que debe usar options->select para seleccionar y flickr_images
 export const getAllImgRoc = async()=>{
     let config = {
         method: "POST", 
